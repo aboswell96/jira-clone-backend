e233cd911687fe53457929126a9a6fa7dfdbb094
Alex Boswell <aboswell778@gmail.com>
2023-03-04 20:00:22 -0800

    change port to 3001

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ app.post("/comments", async (req, res) => {
 
 // start the server
 const host = "0.0.0.0";
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, host, async () => {
   console.log("listening on host=" + host + ",port=" + port);
 });
